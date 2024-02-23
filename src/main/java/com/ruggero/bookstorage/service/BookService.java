bd0aa65c70fd599dfa0d14685fc11e79976e7f5d
S5757G <ruggero.montesi@seb.se>
2024-02-23 09:26:15 +0200

    started refactoring

---
+++ src/main/java/com/ruggero/bookstorage/service/BookService.java
@@ -0,0 +1,32 @@
+package com.ruggero.bookstorage.service;
+
+import com.ruggero.bookstorage.entities.Book;
+import com.ruggero.bookstorage.entities.errorsandexception.IllegalBarcodeException;
+import com.ruggero.bookstorage.entities.errorsandexception.RepeatedBarcodeException;
+import com.ruggero.bookstorage.repos.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+    private final BookRepository repository;
+
+    public Book create(Book book) {
+        validateBarcode(book.getBarcode());
+        return repository.save(book);
+
+    }
+
+    private void validateBarcode(int inputBarcode) {
+        if (inputBarcode < 0) {
+            throw new IllegalBarcodeException("Negative Barcode", inputBarcode);
+        }
+
+        repository.findByBarcode(inputBarcode).stream()
+                .findAny()
+                .ifPresent(book -> {
+                    throw new RepeatedBarcodeException("Barcode " + inputBarcode + " is already present", inputBarcode);
+                });
+    }
+}
