13c51e97326c6a2aeb137c682a3330d3f17ef6d3
Ruggero Montesi <ruggero.montesi1@gmail.com>
2024-02-25 08:09:04 +0200

    improve TestHelper

--- src/test/java/com/ruggero/bookstorage/integration/BookIntegrationTest.java
+++ src/test/java/com/ruggero/bookstorage/integration/BookIntegrationTest.java
@@ -9,20 +9,16 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@AutoConfigureMockMvc
 public class BookIntegrationTest {
     private static final String BOOKS_CREATE_URL = "http://localhost:8080/books/create";
     @Qualifier("bookService")
@@ -30,13 +26,8 @@ public class BookIntegrationTest {
     private BookUseCase service;
 
     @Test
-    void shouldSaveBookRestTemplate() {
+    void shouldCreateBook() {
         RestTemplate restTemplate = new RestTemplate();
-        List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setSupportedMediaTypes(List.of(MediaType.APPLICATION_JSON));
-        messageConverters.add(converter);
-        restTemplate.setMessageConverters(messageConverters);
 
         Book book = Book.builder()
                 .title("Java A Beginner's Guide")
@@ -49,9 +40,14 @@ public class BookIntegrationTest {
         HttpEntity<Book> request = new HttpEntity<>(book);
         Book response = restTemplate.postForObject(BOOKS_CREATE_URL, request, Book.class);
 
-        assertThat(response).isNotNull();
-
         List<Book> books = service.findAll();
+
         assertThat(books).hasSize(1);
+        assertAll(
+                () -> assertThat(response).isNotNull(),
+                () -> assertThat(books).hasSize(1),
+                () -> assertThat(books.get(0)).isEqualTo(book)
+        );
     }
+
 }
--- src/test/java/com/ruggero/bookstorage/service/TestHelper.java
+++ src/test/java/com/ruggero/bookstorage/service/TestHelper.java
@@ -5,43 +5,58 @@ import com.ruggero.bookstorage.entities.Book;
 import java.util.List;
 
 public class TestHelper {
-    private static Book getBook1() {
+
+    public static final String JAVA_A_BEGINNER_S_GUIDE = "Java A Beginner's Guide";
+    public static final String SCHILDT = "Schildt";
+    public static final String OCA_STUDY_GUIDE = "OCA Study Guide";
+    public static final String BOYARSKY_SELIKOFF = "Boyarsky Selikoff";
+    public static final String JAVA_THE_COMPLETE_REFERENCE = "Java The Complete Reference";
+    public static final int QUANTITY_10 = 10;
+    public static final int QUANTITY_1 = 1;
+    public static final int BARCODE_1 = 1;
+    public static final double PRICE_1_0 = 1.0;
+    public static final int BARCODE_2 = 2;
+    public static final double PRICE_2_0 = 2.0;
+    public static final int BARCODE_3 = 3;
+    public static final double PRICE_3_0 = 3.0;
+
+    public static Book getBook() {
         return Book.builder()
-                .title("Java A Beginner's Guide")
-                .author("Schildt")
-                .barcode(1)
-                .quantity(10)
-                .price(1.0)
+                .title(JAVA_A_BEGINNER_S_GUIDE)
+                .author(SCHILDT)
+                .barcode(BARCODE_1)
+                .quantity(QUANTITY_10)
+                .price(PRICE_1_0)
                 .build();
     }
 
     private static Book getBook2() {
         return Book.builder()
-                .title("OCA Study Guide")
-                .author("Boyarsky Selikoff")
-                .barcode(2)
-                .quantity(10)
-                .price(2.0)
+                .title(OCA_STUDY_GUIDE)
+                .author(BOYARSKY_SELIKOFF)
+                .barcode(BARCODE_2)
+                .quantity(QUANTITY_10)
+                .price(PRICE_2_0)
                 .build();
     }
 
     private static Book getBook3() {
         return Book.builder()
                 .title("OCA Practice Test")
-                .author("Boyarsky Selikoff")
-                .barcode(3)
-                .quantity(10)
-                .price(3.0)
+                .author(BOYARSKY_SELIKOFF)
+                .barcode(BARCODE_3)
+                .quantity(QUANTITY_10)
+                .price(PRICE_3_0)
                 .build();
     }
 
     private static Book getBook4() {
         return Book.builder()
-                .title("Java The Complete Reference")
-                .author("Schildt")
+                .title(JAVA_THE_COMPLETE_REFERENCE)
+                .author(SCHILDT)
                 .barcode(4)
-                .quantity(1)
-                .price(3.0)
+                .quantity(QUANTITY_1)
+                .price(PRICE_3_0)
                 .build();
     }
 
@@ -50,8 +65,8 @@ public class TestHelper {
                 .title("Java OCA JAVA SE 8 Programmer I Certification Guide")
                 .author("Mala Gupta")
                 .barcode(5)
-                .quantity(1)
-                .price(2.0)
+                .quantity(QUANTITY_1)
+                .price(PRICE_2_0)
                 .build();
     }
 
@@ -60,14 +75,14 @@ public class TestHelper {
                 .title("OCA Java Programmer 8 Fundamentals 1Z0-808: OCAJP 8")
                 .author("Hanumant Deshmukh")
                 .barcode(6)
-                .quantity(1)
-                .price(1.0)
+                .quantity(QUANTITY_1)
+                .price(PRICE_1_0)
                 .build();
     }
 
     public static List<Book> getBooks() {
         return List.of(
-                getBook1(),
+                getBook(),
                 getBook2(),
                 getBook3(),
                 getBook4(),
